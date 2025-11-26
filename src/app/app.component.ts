4426a4565240b8232f15cb3838bbd4175f0e7e10
harry3574 <harry357.gaming@gmail.com>
2025-11-26 10:36:26 +0100

    first push

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive
+  ]
+})
+export class AppComponent {}
