180d8ac32ed3809eb952b6aa182632a6c3b9a68d
harry3574 <harry357.gaming@gmail.com>
2025-12-17 15:46:13 +0100

    Added TestBed for task.serice.ts

--- src/app/services/task.service.spec.ts
+++ src/app/services/task.service.spec.ts
@@ -1,16 +1,49 @@
 import { TestBed } from '@angular/core/testing';
-
 import { TaskService } from './task.service';
 
 describe('TaskService', () => {
   let service: TaskService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [TaskService]
+    });
+
     service = TestBed.inject(TaskService);
+
+    // Reset tasks manually
+    service['tasks'] = [];
+    service['tasksSubject'].next([]);
   });
 
-  it('should be created', () => {
+  it('devrait être créé', () => {
     expect(service).toBeTruthy();
   });
+
+  it('devrait ajouter une tâche', (done) => {
+    service.addTask('Apprendre les tests');
+
+    service.tasks$.subscribe(tasks => {
+      expect(tasks.length).toBe(1);
+      expect(tasks[0].title).toBe('Apprendre les tests');
+      done();
+    });
+  });
+
+  it('devrait supprimer une tâche', (done) => {
+    service.addTask('Tâche temporaire');
+    let taskId: number;
+
+    service.tasks$.subscribe(tasks => {
+      if (tasks.length > 0) taskId = tasks[0].id;
+    });
+
+    service.removeTask(taskId!);
+
+    service.tasks$.subscribe(tasks => {
+      expect(tasks.length).toBe(0);
+      done();
+    });
+  });
+
 });
