b4c8a6c7a318e6aa90b9ea399acf3ecae6f272fb
harry3574 <harry357.gaming@gmail.com>
2026-01-23 09:46:40 +0100

    Auto-generated commit

---
+++ browser/chunk-LWV7RA5Z.js
@@ -0,0 +1,1 @@
+import{Ga as n,Ha as a,Sa as p,ua as o}from"./chunk-Q4VYQGJV.js";import"./chunk-2NFLSA4Y.js";var i=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=o({type:e,selectors:[["app-about"]],decls:2,vars:0,template:function(t,m){t&1&&(n(0,"p"),p(1,"about works!"),a())},encapsulation:2})};export{i as AboutComponent};
---
+++ browser/chunk-TP2334YR.js
@@ -0,0 +1,1 @@
+import"./chunk-2NFLSA4Y.js";var t=[{path:"",loadComponent:()=>import("./chunk-LWV7RA5Z.js").then(o=>o.AboutComponent)}];export{t as default};
